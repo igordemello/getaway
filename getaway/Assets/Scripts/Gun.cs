2df600eb0f765acd92f5b4dd33c764d09b85b7a2
Igor Mello <igor.oliveira.mello@gmail.com>
2025-10-11 19:38:33 -0300

    Descrição
    Configuração de Ammo - Fácil
    Configuração de tiro para shotgun e Implementação da animação de cooldown shotgun - Média

--- getaway/Assets/Scripts/Gun.cs
+++ getaway/Assets/Scripts/Gun.cs
@@ -1,28 +1,32 @@
 using DG.Tweening.Core.Easing;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
+using TMPro;
 
 public class Gun : MonoBehaviour
 {
-    public enum FireMode { SemiAuto, FullAuto }
+    public enum FireMode { SemiAuto, FullAuto, Shotgun }
 
     [Header("Gun Settings")]
-    public FireMode fireMode = FireMode.SemiAuto;
+    public FireMode fireMode = FireMode.Shotgun;
     public float damage = 10f;
     public float range = 100f;
     public float fireRate = 0.2f;
-    public float recoilAmount = 2f;
-    public float recoilRecovery = 5f;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
 
     [Header("References")]
     public Camera fpsCam;
-    public GunUIRecoil uiRecoil;
     public GameObject impact;
+    public Animator animator;
+    public TextMeshProUGUI debugAmmo;
 
     private PlayerControls controls;
     private bool fireInput;
     private float nextTimeToFire = 0f;
-    private float currentRecoil = 0f;
 
     private void Awake()
     {
@@ -35,9 +39,16 @@ public class Gun : MonoBehaviour
     private void OnEnable() => controls.Enable();
     private void OnDisable() => controls.Disable();
 
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
     void Update()
     {
-        switch(fireMode)
+        debugAmmo.text = $"Ammo:\n{currentAmmo}/{maxAmmo}";
+
+        switch (fireMode)
         {
             case FireMode.SemiAuto:
                 if (fireInput)
@@ -54,19 +65,51 @@ public class Gun : MonoBehaviour
                     Shoot();
                 }
                 break;
-        }
 
-        if (currentRecoil > 0f)
-        {
-            float recover = recoilRecovery * Time.deltaTime;
-            currentRecoil = Mathf.Max(0, currentRecoil - recover);
-            fpsCam.transform.localRotation = Quaternion.Euler(-currentRecoil, 0f, 0f);
+            case FireMode.Shotgun:
+                if (fireInput && Time.time >= nextTimeToFire)
+                {
+                    fireInput = false;
+                    StartCoroutine(DoubleBarrelShot());
+                }
+                break;
         }
 
     }
 
+    private IEnumerator DoubleBarrelShot()
+    {
+        if (currentAmmo <= 0)
+        {
+            Debug.Log("Sem bala lerdao");
+            yield break;
+        }
+
+        nextTimeToFire = Time.time + fireRate;
+
+        Shoot();
+        Shoot();
+
+        yield return new WaitForSeconds(0.1f);
+
+        animator.SetTrigger("Shoot");
+
+        yield return new WaitForSeconds(0.8f);
+
+        nextTimeToFire = Time.time;
+    }
+
     void Shoot()
     {
+        if (currentAmmo <= 0)
+        {
+            Debug.Log("Sem bala lerdao");
+            return;
+        }
+
+
+        currentAmmo--;
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
@@ -82,13 +125,5 @@ public class Gun : MonoBehaviour
             GameObject impactGO = Instantiate(impact, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(impactGO,1f);
         }
-
-        // recoil
-        if (uiRecoil != null)
-        {
-            uiRecoil.DoRecoil();
-        }
-        currentRecoil += recoilAmount;
-        fpsCam.transform.localRotation = Quaternion.Euler(-currentRecoil, 0f, 0f);
     }
 }
