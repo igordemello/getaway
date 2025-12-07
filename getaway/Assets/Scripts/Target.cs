4f2692f15e7057c2a14b70fe939f8be6c6f2a981
Ninguem <arthurpellenz100@gmail.com>
2025-12-07 04:23:20 -0300

    ignorar

--- getaway/Assets/Scripts/Target.cs
+++ getaway/Assets/Scripts/Target.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Threading;
 
 public class Target : MonoBehaviour
 {
@@ -11,8 +13,23 @@ public class Target : MonoBehaviour
     public GameObject other;
     private ExplosiveEnemy explosive;
 
+    [Header("Damage Overlay")]
+    public Image damageOverlay;
+    public float duration;
+    public float fadeSpeed;
+    private float durationTimer;
+    private float maxOverlayAlpha = 0.8f;
+    private float damageToAlphaFactor = 0.5f;
+    private float currentAlpha;
+    private void Start()
+    {
+        if(damageOverlay)
+            damageOverlay.color = new Color(damageOverlay.color.r, damageOverlay.color.g, damageOverlay.color.b, 0);
+    }
+
     private void Update()
     {
+
         if (CompareTag("Player"))
             health_UI.text = $"Health:\n{health}";
 
@@ -33,19 +50,50 @@ public class Target : MonoBehaviour
             Die();
         }
 
+        if (CompareTag("Player"))
+        {
+            if (health <= 15 && health > 0)
+            {
+                float pulse = Mathf.Abs(Mathf.Sin(Time.time * 2.5f)) * 0.9f;
+                SetOverlayAlpha(pulse);
+                return;
+            }
+            if (damageOverlay.color.a > 0)
+            {
+                currentAlpha = Mathf.Lerp(currentAlpha, 0, Time.deltaTime * fadeSpeed);
+                SetOverlayAlpha(currentAlpha);
+            }
+        }
+
     }
 
     public void TakeDamage(float amount)
     {
+        if (CompareTag("Player"))
+        {
+            durationTimer = 0;
+            float targetAlpha = Mathf.Clamp(damageOverlay.color.a + amount * damageToAlphaFactor, 0, maxOverlayAlpha);
+
+            SetOverlayAlpha(targetAlpha);
+        }
+            
+
         health -= amount;
         if (health < 0f) health = 0f;
 
+    }
+    private void SetOverlayAlpha(float alpha)
+    {
+        currentAlpha = alpha;
+        damageOverlay.color = new Color(damageOverlay.color.r, damageOverlay.color.g, damageOverlay.color.b, currentAlpha);
     }
     IEnumerator DeathDelay()
     {
         yield return new WaitForSeconds(0.3f);
         SceneManager.LoadScene("DerrotaMenu");
     }
+
+   
 
     void Die()
     {
